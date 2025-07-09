97986393caddff2fdb93912dece98035281e6f26
Gašper Melanšek <77798581+GhostGapy@users.noreply.github.com>
2025-07-09 12:59:34 +0200

    Dodan sistemski loader

--- app/src/main/java/com/example/testtasktwo/activity/MainActivity.kt
+++ app/src/main/java/com/example/testtasktwo/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.testtasktwo.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -12,16 +13,20 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.testtasktwo.viewmodel.AlbumViewModel
 import com.example.testtasktwo.R
 import com.example.testtasktwo.adapter.AlbumAdapter
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: AlbumViewModel
     private lateinit var adapter: AlbumAdapter
+    private lateinit var loadingProgressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        loadingProgressBar = findViewById(R.id.loadingProgressBar)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -36,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.albumRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         adapter = AlbumAdapter(emptyList()) { album ->
+            loadingProgressBar.visibility = View.VISIBLE
             viewModel.onAlbumSelected(album)
         }
         recyclerView.adapter = adapter
@@ -47,6 +53,7 @@ class MainActivity : AppCompatActivity() {
 
         // navigacija do PhotoActivity ob kliku na album z posiljanjem
         viewModel.navigateToPhotos.observe(this) { selectedAlbum ->
+            loadingProgressBar.visibility = View.GONE
             val intent = Intent(this, PhotoActivity::class.java)
             intent.putExtra("album_id", selectedAlbum.id)
             intent.putExtra("album_title", selectedAlbum.title)
